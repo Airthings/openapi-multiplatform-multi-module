9c2fb992f4e88ffea26c62d2e226956e8cf67e67
Tal Kirshboim <tal.kirshboim@gmail.com>
2021-11-30 13:52:40 +0100

    Add user agent and logging to HTTP Client config

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -21,6 +21,7 @@ kotlin {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
                 implementation(project(":openapi-generator:samples:client:petstore:kotlin-multiplatform"))
+                implementation("io.ktor:ktor-client-logging:1.6.3")
             }
         }
         val commonTest by getting {
--- shared/src/commonMain/kotlin/com/airthings/openapi/test/Greeting.kt
+++ shared/src/commonMain/kotlin/com/airthings/openapi/test/Greeting.kt
@@ -1,11 +1,34 @@
 package com.airthings.openapi.test
 
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.features.UserAgent
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
 import org.openapitools.client.apis.PetApi
 
 class Greeting {
+
+    private val petApi = PetApi(httpClientConfig = createHttpClientConfig())
+
+    private fun createHttpClientConfig(): ((HttpClientConfig<*>) -> Unit) =
+            { clientConfig ->
+                clientConfig.install(UserAgent) {
+                    agent = "my-pet-store-app/${Platform().platform}"
+                }
+                clientConfig.install(Logging) {
+                    logger = object : Logger {
+                        override fun log(message: String) {
+                            println("HttpClient: $message")
+                        }
+                    }
+                    level = LogLevel.ALL
+                }
+            }
+
     @Throws(Throwable::class)
     suspend fun greetPet(): String {
-        val petName = PetApi().findPetsByStatus(listOf("available")).body().firstOrNull()?.name ?: "??"
+        val petName = petApi.findPetsByStatus(listOf("available")).body().firstOrNull()?.name ?: "??"
         return "Hello $petName! From ${Platform().platform}."
     }
 }
